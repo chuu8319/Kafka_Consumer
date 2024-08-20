738e24c6fae1f24ce53488a69e645b24729453ed
chuu8319 <chuu8319@gmail.com>
2024-08-20 16:04:40 +0900

    JSON

--- src/main/java/com/mykafka/consumer/KafkaMessageListener.java
+++ src/main/java/com/mykafka/consumer/KafkaMessageListener.java
@@ -1,5 +1,6 @@
 package com.mykafka.consumer;
 
+import com.mykafka.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,8 +11,26 @@ public class KafkaMessageListener {
 
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consume(String message) {
-        log.info("Consumer consume the message {} ", message);
+//    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+//    public void consume(String message) {
+//        log.info("consumer1 consume the message {} ", message);
+//    }
+//    @KafkaListener(topics = "${topic.name}",groupId = "${spring.kafka.consumer.group-id}")
+//    public void consume2(String message) {
+//        log.info("consumer2 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "${topic.name}",groupId = "${spring.kafka.consumer.group-id}")
+//    public void consume3(String message) {
+//        log.info("consumer3 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "${topic.name}",groupId = "${spring.kafka.consumer.group-id}")
+//    public void consume4(String message) {
+//        log.info("consumer4 consume the message {} ", message);
+//    }
+    @KafkaListener(topics = "${topic.name}",groupId = "${spring.kafka.consumer.group-id}")
+    public void consumeObject(Customer customer) {
+        log.info("consumer consume the Customer Object {} ", customer.toString());
     }
 }
